80a6217ab4c77ad338d94fdad88a7c16052ce1e3
Josef Bauer <josef.bauer.1st@gmail.com>
2023-05-03 01:59:39 +0200

    Remove SolidJS logic from auth-service

--- lib/supabase/auth-service.ts
+++ lib/supabase/auth-service.ts
@@ -47,94 +47,21 @@ export async function logout() {
 }
 
 export async function getUserId(): Promise<string | undefined> {
-  if (!isServer) {
-    const session = await getSession();
-    return session.userId;
-  }
-
-  const ctx = useServerContext();
-
-  if (!ctx.request) {
-    return;
-  }
-
-  const tokens = await extractTokensFromRequest(ctx.request);
-
-  if (tokens) {
-    await setSupabaseTokens(tokens);
-  }
-
-  try {
-    const { payload } = await jwtVerify(tokens?.accessToken || "", JWT_KEY);
-    return payload.sub;
-  } catch {
-    return;
-  }
-}
-
-type SupabaseTokens = {
-  accessToken: string;
-  refreshToken: string;
-};
-
-export async function extractTokensFromRequest(request: Request): Promise<SupabaseTokens | undefined> {
-  const cookies = request.headers.get("cookie");
-
-  if (!cookies) {
-    return;
-  }
-
-  const accessTokenString = cookies.split(";").find((value) => value.includes("my-access-token"));
-  const refreshTokenString = cookies.split(";").find((value) => value.includes("my-refresh-token"));
-
-  if (!accessTokenString || !refreshTokenString) {
-    return;
-  }
-
-  const accessToken = accessTokenString.split("=")[1].trim();
-  const refreshToken = refreshTokenString.split("=")[1].trim();
-
-  return {
-    accessToken,
-    refreshToken
-  };
-}
-
-export async function setSupabaseTokens(tokens: SupabaseTokens) {
-  const { data, error } = await supabase.auth.getSession();
-
-  if (error) {
-    console.error("Can't set supabase session in server:", error.message);
-    return;
-  }
-
-  const currentSession = data.session;
-
-  if (currentSession && currentSession.access_token && currentSession.refresh_token) {
-    return;
-  }
-
-  await supabase.auth.setSession({
-    access_token: tokens.accessToken,
-    refresh_token: tokens.refreshToken
-  });
+  const session = await getSession();
+  return session.userId;
 }
 
 supabase.auth.onAuthStateChange((event, session) => {
   if (event === "SIGNED_OUT") {
     const expires = new Date(0).toUTCString();
-    if (!isServer) {
-      document.cookie = `my-access-token=; path=/; expires=${expires}; SameSite=Lax; secure`;
-      document.cookie = `my-refresh-token=; path=/; expires=${expires}; SameSite=Lax; secure`;
-    }
-    sessionStore.userId = undefined;
+    document.cookie = `my-access-token=; path=/; expires=${expires}; SameSite=Lax; secure`;
+    document.cookie = `my-refresh-token=; path=/; expires=${expires}; SameSite=Lax; secure`;
+    // sessionStore.userId = undefined;
   } else if (event === "SIGNED_IN" || event === "TOKEN_REFRESHED") {
     const maxAge = 5 * 24 * 60 * 60; // 5 days
-    if (!isServer) {
-      document.cookie = `my-access-token=${session?.access_token}; path=/; max-age=${maxAge}; SameSite=Lax; secure`;
-      document.cookie = `my-refresh-token=${session?.refresh_token}; path=/; max-age=${maxAge}; SameSite=Lax; secure`;
-    }
-    sessionStore.userId = session?.user.id;
-    sessionStore.isDealer = session?.user.user_metadata.isDealer;
+    document.cookie = `my-access-token=${session?.access_token}; path=/; max-age=${maxAge}; SameSite=Lax; secure`;
+    document.cookie = `my-refresh-token=${session?.refresh_token}; path=/; max-age=${maxAge}; SameSite=Lax; secure`;
+    // sessionStore.userId = session?.user.id;
+    // sessionStore.isDealer = session?.user.user_metadata.isDealer;
   }
 });
