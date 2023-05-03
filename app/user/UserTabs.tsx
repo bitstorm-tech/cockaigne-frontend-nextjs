9caecdb8aa7e0112b84bab9667471ddc2835de69
Josef Bauer <josef.bauer.1st@gmail.com>
2023-05-03 16:25:26 +0200

    Fix infinitive loop

---
+++ app/user/UserTabs.tsx
@@ -0,0 +1,47 @@
+"use client";
+import EmptyContent from "@/components/ui/EmptyContent";
+import FireIcon from "@/components/ui/icons/FireIcon";
+import HeartIcon from "@/components/ui/icons/HeartIcon";
+import StarIcon from "@/components/ui/icons/StarIcon";
+import Link from "next/link";
+import { useState } from "react";
+
+export default function UserTabs() {
+  const [tabIndex, setTabIndex] = useState(0);
+  return (
+    <>
+      <div className="tabs mb-1 mt-6 flex max-h-8">
+        <button onClick={() => setTabIndex(0)} className={`tab-bordered tab grow ${tabIndex === 0 && "tab-active"}`}>
+          <StarIcon outline={tabIndex !== 0} />
+        </button>
+        <button onClick={() => setTabIndex(1)} className={`tab-bordered tab grow ${tabIndex === 1 && "tab-active"}`}>
+          <FireIcon outline={tabIndex !== 1} />
+        </button>
+        <button onClick={() => setTabIndex(2)} className={`tab-bordered tab grow ${tabIndex === 2 && "tab-active"}`}>
+          <HeartIcon outline={tabIndex !== 2} />
+        </button>
+      </div>
+      <button onClick={() => setTabIndex(Math.random)}>Click mich!</button>
+      <p>{tabIndex}</p>
+      {/*{tabIndex === 0 && <UserDealList deals={deals()} emptyContent={emptyContent} />}*/}
+      {/*{tabIndex === 1 && <UserHotDealList />}*/}
+      {/*{tabIndex === 2 && <UserFavoriteDealerList />}*/}
+    </>
+  );
+}
+
+const emptyContent = (
+  <EmptyContent>
+    <p>Aktuell gibt es leider keine Deals in deiner Nähe :(</p>
+    <p>
+      <Link href="/map?showFilter=true">
+        <u>Filter anpassen</u>
+      </Link>
+      {" oder "}
+      <Link href="/map">
+        <u>Standort ändern</u>
+      </Link>
+      !
+    </p>
+  </EmptyContent>
+);
--- components/user/ProfileImage.tsx
+++ components/user/ProfileImage.tsx
@@ -1,4 +1,5 @@
 import { getProfileImage } from "@/lib/supabase/storage-service";
+import { Suspense } from "react";
 
 export default async function ProfileImage({ size = 6, isDealer = false }) {
   const style = { height: `${size}rem`, width: `${size}rem` };
@@ -8,7 +9,9 @@ export default async function ProfileImage({ size = 6, isDealer = false }) {
   return (
     <div className={`avatar cursor-pointer ${menuOpen && "invisible"}`}>
       <div className="rounded-full ring-2 ring-[#556368]" style={style}>
-        <img loading="lazy" src={profileImageUrl} alt="Profile" />
+        <Suspense>
+          <img loading="lazy" src={profileImageUrl} alt="Profile" />
+        </Suspense>
       </div>
     </div>
   );
